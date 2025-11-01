f686fa9cc7de95fdd2ada6fa8d4b70e59a77c204
Ajitesh Kumar <ajinest6@gmail.com>
2025-10-31 21:23:57 -0400

    we're using sveltekit instead now...

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-red-500 text-center">hi, from aji</h1>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+	plugins: [tailwindcss(), sveltekit()]
 });
