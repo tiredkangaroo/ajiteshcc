3f624cc77630782254e10a68b37048127acf10a6
Ajitesh Kumar <ajinest6@gmail.com>
2025-11-23 18:59:40 -0500

    make star star

--- update.sh
+++ update.sh
@@ -6,20 +6,17 @@ if [[ ! -v HOST_PORT ]]; then
 fi
 
 # docker compose down
-echo -n "docker compose down: "
+echo "** docker compose down **"
 docker compose down
-echo -e "✅"
 
 # update the local repository
-echo -n "git pull: "
+echo "** git pull **"
 git pull
-echo -e "✅"
 
 
 # build and start the containers in detached mode
-echo -n "docker compose up -d --build: "
+echo "** docker compose up -d --build **"
 docker compose up -d --build
-echo -e "✅"
 
 # show the status of the containers
 docker compose ps
